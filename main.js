7cb93672d0a281da790179ae14f183d4cee5178d
vladneyo <vlad27neychev95@gmail.com>
2016-04-15 20:18:26 +0300

    houses move, but score calculates not correct

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // Create our 'main' state that will contain the game
 var mainState = {
+    coef: 2,
+
     preload: function() {
-        // This function will be executed at the beginning
-        // That's where we load the images and sounds
         game.load.image('santa', 'assets/santa.png');
         game.load.image('gift', 'assets/gift.png');
+        game.load.image('house', 'assets/house.png');
 
     },
 
@@ -33,11 +34,25 @@ var mainState = {
         var spaceKey = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
         spaceKey.onDown.add(this.dropGift, this);
 
-    },
+        this.houses = game.add.group();
+        this.gifts = game.add.group();
 
+        this.timer = game.time.events.loop(1500/this.coef, this.addHouse, this);
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0",
+            { font: "30px Arial", fill: "#ffffff" });
+
+    },
     update: function() {
-        // This function is called 60 times per second
-        // It contains the game's logic
+        game.physics.arcade.overlap(this.gifts, this.houses, this.increaseScore, null, this);
+
+    },
+    increaseScore: function(){
+        //console.log(this.gifts);
+        for(var i=0;i < this.gifts.children.length; i++)
+            this.gifts.remove(this.gifts.children[i]);
+        this.score++;
+        this.labelScore.text = this.score;
     },
     restartGame: function() {
         // Start the 'main' state, which restarts the game
@@ -53,13 +68,33 @@ var mainState = {
     },
     dropGift: function() {
         //drop the gift
-        console.log("drop");
-        this.gift = game.add.sprite(this.santa.x, this.santa.y+ this.santa.height, 'gift');
-        game.physics.arcade.enable(this.gift);
-        this.gift.scale.x = 0.15;
-        this.gift.scale.y = 0.15;
+        var gift = game.add.sprite(this.santa.x + this.santa.width / 6, this.santa.y + this.santa.height, 'gift');
+        this.gifts.add(gift);
+        game.physics.arcade.enable(gift);
+        gift.scale.x = 0.1;
+        gift.scale.y = 0.1;
 
-        this.gift.body.gravity.y = 1000;
+        gift.body.gravity.y = 2000*this.coef;
+        gift.checkWorldBounds = true;
+        gift.outOfBoundsKill = true;
+    },
+    addHouse: function(){
+
+        var house = game.add.sprite(1000, 500, 'house');
+        this.houses.add(house);
+        house.scale.x = 0.35;
+        house.scale.y = 0.35;
+
+        // Enable physics on the pipe
+        game.physics.arcade.enable(house);
+
+        // Add velocity to the pipe to make it move left
+        house.body.velocity.x = -400*this.coef;
+        house.body.gravity.y = 0;
+
+        // Automatically kill the pipe when it's no longer visible
+        house.checkWorldBounds = true;
+        house.outOfBoundsKill = true;
     },
 };
 
